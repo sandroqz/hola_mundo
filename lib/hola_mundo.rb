87ed235395f1ba7a6555e36435ea17898023f2c8
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-07-01 04:47:12 -0300

    Added documentation to HolaMundo class

--- lib/hola_mundo.rb
+++ lib/hola_mundo.rb
@@ -1,4 +1,14 @@
+# The main Hola Mundo driver
 class HolaMundo
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> HolaMundo.hi("english")
+  #   => Hello world!
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = :spanish)
     translator = Translator.new(language)
     translator.hi
